9a17669badc99224054cf73121c554c81c0679ca
Kyle Blake <kyle.blake84@gmail.com>
2014-12-29 17:54:23 -0600

    Create ch191--Word_Counting.py

---
+++ easy/ch191--Word_Counting.py
@@ -0,0 +1,28 @@
+from sys import argv
+
+script, filename = argv
+
+
+def clean_word(w):
+    result = ''
+    for letter in w:
+        lower_letter = letter.lower()
+        if lower_letter in 'abcdefghijklmnopqrstuvwxyz':
+            result += lower_letter
+    return result
+
+wordCount = {}
+with open(filename, 'r') as f:
+    for line in f:
+        for word in line.split():
+            word = clean_word(word)
+            if word is not '':
+                if word in wordCount:
+                    wordCount[word] = wordCount.get(word) + 1
+                else:
+                    wordCount[word] = 1
+
+
+wordCount = sorted(wordCount.items(), key=lambda count: count[1], reverse=True)
+for each in wordCount:
+    print each
