773db61890eba5a080dfdda08d42e5043d8891ed
Kyle Blake <kyle.blake84@gmail.com>
2016-04-11 10:55:56 -0500

    Completed challenge 262.

---
+++ easy/Challenge262/Challenge262.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DailyProgrammer
+{
+    public class Challenge262
+    {
+        public Challenge262()
+        {
+        }
+
+        public static void MaybeNumeric(string input)
+        {
+            double test;
+            if (double.TryParse(input, out test))
+            {
+                Debug.WriteLine(test + "(number)");
+            }
+            else
+            {
+                Debug.WriteLine(input + "(string)");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] test = { "123", "44.234", "0x123N" };
+            int x = 1;
+            foreach (string testCase in test)
+            {
+                Debug.Write("Test " + x + ": ");
+                Challenge262.MaybeNumeric(testCase);
+                x++;
+            }
+        }
+    }
+}
+
---
+++ easy/Challenge262/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DailyProgrammer
+{
+    public class Challenge262
+    {
+
+
+        public Challenge262()
+        {
+        }
+
+
+        public static void MaybeNumeric(string input)
+        {
+            double test;
+            if (double.TryParse(input, out test))
+            {
+                Debug.WriteLine(test + "(number)");
+            }
+            else
+            {
+                Debug.WriteLine(input + "(string)");
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            string[] test = { "123", "44.234", "0x123N" };
+
+            foreach (string testCase in test)
+            {
+                Challenge262.MaybeNumeric(testCase);
+            }
+        }
+    }
+}
+
